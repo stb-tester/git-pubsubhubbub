ea2b25714684c79a39c6a8c5768d9e9c99c248a0
William Manley <will@williammanley.net>
2016-04-25 10:50:36 +0100

    Add initial rough-cut implementation of README

---
+++ README.md
@@ -0,0 +1,85 @@
+git-pubsubhubbub
+================
+
+git-pubsubhubbub makes it possible to receive notification when a remote git
+repo changes.  Subscribers use an HTTP POST to register a callback which will be
+POSTed to by the git server once a change occurs.
+
+It is:
+
+* **Easy** to setup and deploy.  git-pubsubhubbub is portable and deployment
+  consists of running a single, self-contained executable from any git repo.
+
+* **Compatible** with the [github pubsubhubbub hooks][1].  If you integrate with
+  git-pubsubhubbub you can reuse that exact same integration with github.  We're
+  hoping to spur a standard way for clients to receive push notifications from
+  git-repos.
+
+* **Secure** - git-pubsubhubbub makes it easy to reuse the security mechanisms
+  and policies you've already set-up around your git repos.  Anyone who has
+  permission read from your git repo can register for callbacks, but no-one
+  else can.  It takes care with the pubsubhubbub protocol to perform validation
+  on requests and to properly sign notifications.
+
+* **Decentralised** - You no-longer have to set-up your central git server to
+  know about every client that could be interested in changes.  Clients register
+  for interest using the same mechanism and via the same security policy as they
+  pull changes - HTTP.  If you can poll for changes you can receive
+  notifications too, so there's no longer an excuse to [poll][2].
+
+[1]: https://developer.github.com/v3/repos/hooks/#pubsubhubbub
+[2]: http://kohsuke.org/2011/12/01/polling-must-die-triggering-jenkins-builds-from-a-git-hook/
+
+Advantages vs. Polling
+----------------------
+
+* Lower latency: subscribers are notified immediately on push and don't have to
+  wait for the next polling interval to find out about a change.  This
+  additional latency that polling requires can cause a considerable increase in
+  the time it takes to get test results, particularly if your tests are fast and
+  your polling interval is slow.
+
+* Less resource consumption: Polling requires more wakeups and greater load on
+  the clients, servers and networks.
+
+Advantages vs. Custom Hooks
+---------------------------
+
+* You don't need to modify your git server configuration every time you add,
+  update or remove a client.  Everyone who can poll a repo can register for
+  callbacks.
+
+* Simpler security: By applying the same security policy to git-pubsubhubbub
+  as gitweb there's only one security policy to maintain and you don't need to
+  modify and audit your manually set-up hooks every time you change your policy.
+
+Quick Start
+===========
+
+1. Download git-pubsubhubbub for [Linux], [OS X] or [Windows]
+2. Change to your git repo directory with `cd your-git-repo`
+2. Run `git-pubsubhubbub`
+
+You can now subscribe for changes on
+http://localhost:8080/your-git-repo/events/push .
+
+[Linux]: 
+[OS X]: 
+[Windows]: 
+
+Usage
+=====
+
+    git-pubsubhubbub [-listen-address=LISTEN_ADDRESS] [-address-prefix=ADDRESS_PREFIX] [-projectroot=ROOT]
+
+Security
+========
+
+Integration
+===========
+
+With gitweb
+-----------
+
+Licence
+=======
--- gitpubhook/main.go
+++ gitpubhook/main.go
@@ -10,32 +10,9 @@ import (
 	"os/exec"
 )
 
-func serve(address, prefix) {
-}
-
-func hook(endpoint, nonce) {
-	req, err := http.NewRequest("POST", endpoint, os.Stdin)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to create POST request for %s", endpoint)
-		return 1
-	}
-	req.Header.Add("X-Git-Pubsubhubbub-Nonce", nonce)
-	req.Header.Add("Content-Type", "text/plain")
-
-	client := http.Client{}
-	resp, err := client.Do(req)
-	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-		fmt.Fprintln(os.Stderr, "hook failed to read HTTP response")
-		return 1
-	}
-}
-
-func main() {
-	prefixp := flag.String("prefix", "", "HTTP endppoint prefix");
+func serve() int {
+	prefixp := flag.String("prefix", "", "HTTP endpoint prefix");
 	address := flag.String("address", ":8080", "Address to listen on");
-
-	nonce := flag.String("-nonce", "", "HTTP endppoint prefix");
-	url := flag.String("-address", ":8080", "Address to listen on");
 
 	flag.Parse();
 
@@ -66,7 +43,33 @@ func main() {
 		prefix = *prefixp;
 	} else {
 		prefix = "/" + repo_name + "/events"
-	} 
+	}
+
+	/* Generate nonce that will be used to confirm that HTTP POSTs in to this
+	 * process are from the git hooks.  This is saved with great care with
+	 * respect to permissions. */
+	nonce_bytes := make([]byte, 32)
+	if _, err := rand.Reader(nonce); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to generate nonce: %s", err)
+		return 1
+	}
+	nonce := hex.EncodeToString(nonce_bytes);
+
+	/* We want users who can write to the repo to be able to call the hook, but
+	 * no-one else, so copy the write mode on the git directory to the read mode
+	 * of the nonce file */
+	if git_stat, err := os.Stat(git_dir); err != nil {
+		fmt.Fprintln(os.Stderr, "statting git dir failed: %s", err)
+		return 1
+	}
+
+	nonce_mode := 0200 | ((git_stat.Mode() & 0222) << 1)
+	err := writeFileExcl("git-pubsubhubbub-nonce.txt", nonce, nonce_mode,
+	                     git_stat.Uid, git_stat.Gid)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to write pubsubhubbub nonce: %s", err)
+		return 1
+	}
 
 	fmt.Printf("Serving pubsubhubbub on http://%s%s/push\n", *address, prefix)
 
@@ -87,10 +90,117 @@ func main() {
 		
 	}
 
-	http.HandleFunc(prefix + "/push/hooks/pre-receive", handleHookPost)
+	http.HandleFunc(prefix + "/push/hooks/pre-receive", handleHook(nonce))
 
 	if err := http.ListenAndServe(hub.address, nil); err != nil {
 		fmt.Fprintln(os.Stderr, "Serving failed: ", err);
 		os.Exit(1);
+	}
+}
+
+
+func writeFileExcl(name string, data []byte, perm os.FileMode, uid int, gid int) error {
+	if err := os.RemoveAll(name); err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(name, os.O_WRONLY | os.O_CREATE | os.O_EXCL, 0200)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if statinfo, err := f.Stat(); err != nil {
+		return err
+	}
+
+	if statinfo.Uid != uid || statinfo.Gid != gid {
+		if err := f.chown(uid, gid); err != nil {
+			fmt.Fprintln(os.Stderr, "Cannot create nonce file: We must be running with same uid and gid as the git dir, be running as root or have the CAP_CHOWN capability: %s", err);
+			os.Remove(name)
+			return err
+		}
+	}
+
+	if err := f.chmod(perm); err != nil {
+		os.Remove(name)
+		return err
+	}
+
+	if _, err := f.Write(data); err != nil {
+		os.Remove(name)
+		return err
+	}
+	return nil
+}
+
+/* Called by git when a git-push is complete.  We need to be able to contact
+ * our server to pass on the information that git sends us such that it can
+ * distribute that data to all the subscribers.  See `man githooks` for more
+ * details. */
+func hook() int {
+	if nonce, err := ioutil.ReadFile("git-pubsubhubbub-nonce.txt"); err != nil {
+		fmt.Fprintln(os.Stderr, "hook failed to read pubsubhubbub nonce")
+		return 1
+	}
+
+	req, err := http.NewRequest("POST", endpoint, os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to create POST request for %s", endpoint)
+		return 1
+	}
+	req.Header.Add("X-Git-Pubsubhubbub-Nonce", nonce)
+	req.Header.Add("Content-Type", "application/octet-stream")
+
+	client := http.Client{}
+	if resp, err := client.Do(req); err != nil {
+		fmt.Fprintln(os.Stderr, "HTTP POST to pubsubhubbub hub failed")
+		return 1
+	}
+
+	if resp.StatusCode != 200 {
+		/* We're in an error state, don't care about further errors: */
+		io.Copy(os.Stderr, resp.Body)
+		return 1
+	}
+
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "hook failed to read HTTP response")
+		return 1
+	}
+	return 0
+}
+
+func handleHook(nonce string, topic string, hub *Hub) {
+	/* Called in response to receiving a POST from hook() */
+	return func (w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		r_nonce := r.Header.Get("X-Git-Pubsubhubbub-Nonce")
+		if r_nonce != nonce {
+			w.WriteHeader(403)
+			w.Write("Incorrect Nonce")
+			return
+		}
+
+		if payload, err := ioutil.ReadAll(r.Body); err != nil {
+			w.WriteHeader(400)
+			w.Write("Failed to read payload from body")
+			return
+		}
+
+		if err := hub.Notify(topic, "application/json", payload); err != nil {
+			w.WriteHeader(500)
+			w.Write("Notifing subscribers failed")
+			return
+		}
+		w.WriteHeader(200)
+	}
+}
+
+func main() {
+	if path.Base(os.Args[0]).HasPrefix("post-receive") {
+		os.Exit(hook())
+	} else {
+		os.Exit(serve())
 	}
 }
