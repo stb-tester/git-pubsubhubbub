41b020736233967b5d25325f498f0d610054f6f0
William Manley <will@williammanley.net>
2016-04-26 17:34:27 +0100

    Add pubsubhubbub client for testing
    Based on the gohubbub library.  We will need more than just this for
    complete testing but this gives us a pretty good starting point.

---
+++ testclient/testclient.go
@@ -0,0 +1,18 @@
+package main
+
+import "flag"
+import "github.com/dpup/gohubbub"
+import "log"
+
+func main() {
+	hubp := flag.String("hub", "http://localhost:8080/hub", "Hub to connect to");
+	topicp := flag.String("topic", "http://localhost:8080/testrepo/events/push", "Topic to subscribe to");
+	self := flag.String("self", "localhost:8000", "Address to call us back on")
+	flag.Parse()
+
+	client := gohubbub.NewClient(*self, "Testing")
+	client.Subscribe(*hubp, *topicp, func(contentType string, body []byte) {
+		log.Printf("Received callback %s\n%s\n", contentType, string(body))  
+	})
+	client.StartAndServe("0.0.0.0", 54321)
+}
